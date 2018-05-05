7860eb02b4fd8bcb0033f9a2359553dc1cd2796d
Aurélien Perrier <a.perrier89@gmail.com>
2018-05-05 16:28:15 +0200

    Adding notes for variables

--- variables.tf
+++ variables.tf
@@ -1,31 +1,68 @@
+# For more details please see the following pages :
+# https://www.terraform.io/docs/providers/google/r/container_cluster.html
+# https://www.terraform.io/docs/providers/google/r/container_node_pool.html
+# https://www.terraform.io/docs/providers/google/d/google_container_engine_versions.html
+# https://www.terraform.io/docs/providers/google/d/google_container_registry_repository.html
+
 ##########################
 ###         GKE        ###
 ##########################
 
+# Parameters authorized:
+# name (mandatory)
+# zone (mandatory)
+# env (mandatory)
 variable "general" {
   type        = "map"
   description = "Global parameters"
 }
 
+# Parameters authorized:
+# username (mandatory)
+# password (mandatory)
+# network (default: default)
+# subnetwork (default: default)
+# disable_horizontal_pod_autoscaling (default: false)
+# disable_http_load_balancing (default: false)
+# disable_kubernetes_dashboard (default: false)
+# disable_network_policy_config (default: true)
+# enable_kubernetes_alpha (default: false)
+# enable_legacy_abac (default: false)
+# private (default: false)
+# maintenance_window (default: 4:30)
+# version (default: Data resource)
+# monitoring_service (default: none)
+# logging_service (default: logging.googleapis.com)
+# disk_size_gb (default: 10)
+# image (default: COS)
+# local_ssd_count (default: 0)
+# oauth_scopes (default: https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring)
+# machine_type (default: n1-standard-1)
+# preemptible (default: false)
 variable "master" {
   type        = "map"
-  default     = {}
   description = "Kubernetes master parameters to initialize"
 }
 
+# Parameters authorized:
+# node_count (default: 3)
+# remove (default: true)
 variable "default_node_pool" {
   type        = "map"
   default     = {}
   description = "Default pool setting"
 }
 
-# https://www.terraform.io/docs/providers/google/r/container_cluster.html#additional_zones
-variable "default_node_pool_additional_zones" {
-  type        = "list"
-  default     = []
-  description = "The list of additional Google Compute Engine locations in which the cluster's nodes should be located. If additional zones are configured, the number of nodes specified in default_node_pool[node_count] is created in all specified zones"
-}
-
+# Parameters authorized:
+# node_count (default: 3)
+# machine_type (default: n1-standard-1)
+# disk_size_gb (default: 10)
+# local_ssd_count (default: 0)
+# oauth_scopes (default: https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring)
+# min_node_count (default: 1)
+# max_node_count (default: 3)
+# auto_repair (default: true)
+# auto_upgrade (default: true)
 variable "node_pool" {
   type        = "list"
   default     = []
