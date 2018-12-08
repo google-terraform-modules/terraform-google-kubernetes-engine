7407c3ab4ffcfb3d2a4dea1aa11b1e105537c6ae
Aurélien PERRIER <a.perrier89@gmail.com>
2018-12-08 14:57:23 +0100

    Fixing #25 client_certificate

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,11 @@ output "client_key" {
   description = "Base64 encoded private key used by clients to authenticate to the cluster endpoint"
 }
 
+output "client_certificate" {
+  value       = "${google_container_cluster.new_container_cluster.master_auth.0.client_certificate}"
+  description = "Base64 encoded public certificate used by clients to authenticate to the cluster endpoint"
+}
+
 output "cluster_ca_certificate" {
   value       = "${google_container_cluster.new_container_cluster.master_auth.0.cluster_ca_certificate}"
   description = "Base64 encoded public certificate that is the root of trust for the cluster"
